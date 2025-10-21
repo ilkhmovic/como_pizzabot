21bdbc35a60dc04d81e9771d97666fd33ef2eeeb
Doston <160345511+ilkhmovic@users.noreply.github.com>
2025-10-21 16:31:51 +0500

    Update print statement from 'Hello' to 'Goodbye'

--- handlers.py
+++ handlers.py
@@ -39,7 +39,7 @@ MERCHANT_ID = os.environ.get('MERCHANT_ID')
 WEBHOOK_HOST = os.environ.get("WEBHOOK_HOST")
 
 # Asosiy joy koordinatalari (o'z joyingiz koordinatalarini qo'ying)
-MAIN_LOCATION = (41.311081, 69.240562)  # Toshkent markazi misol uchun
+MAIN_LOCATION = (41.3654169, 69.3000834)  # Toshkent markazi misol uchun
 
 router = Router()
 
@@ -990,3 +990,4 @@ async def handle_unknown_messages(message: types.Message):
         "Noto'g'ri buyruq. Iltimos, menyudan tugmalardan foydalaning.",
         reply_markup=get_main_keyboard(user_lang)
     )
+
